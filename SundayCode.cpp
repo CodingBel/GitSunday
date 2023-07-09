47d8ee7b6c80fa8908f479acdda972cad5e8b3ac
Abel <belaashu23@gmail.com>
2023-07-09 15:09:04 +0200

    Changes from teh third user

--- SundayCode.cpp
+++ SundayCode.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main (){
     cout << "Hello Bela" << endl; 
     cout << "what a Sunday" << endl; 
+    Cout << "Message From the Third User" << endl; 
 
     return 0; 
 }
